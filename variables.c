e33abc4f350a62591d11128e719204276d8199de
Nicolas CHANGO <nicolas.chango96@gmail.com>
2021-06-03 17:25:30 +0200

    programmes Variables modification 1

--- variables.c
+++ variables.c
@@ -3,7 +3,12 @@
 
 int main(int argc, char *argv[])
 {
-	int nombreDeVies;
+	int nombreDeVies = 5; // Au déparrt, le joueur a 5 vies
+
+	printf("Vous avez %d vies \n",nombreDeVies);
+	printf("**** BAM ****\n");
+	nombreDeVies = 4; //Il vient de perdre une vie !
+	printf("Ah désolé, il ne reste plus que %d vies maintenant ! \n\n", nombreDeVies);
 
 	return 0;
 }
